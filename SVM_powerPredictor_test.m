9ee0f9db1a5e9edce6873cc316cc2c5dffc81830
Hubert Kyeremateng-Boateng <hkboateng@gmail.com>
2018-08-07 22:05:40 -0400

    continue working on project

--- SVM_powerPredictor_test.m
+++ SVM_powerPredictor_test.m
@@ -39,7 +39,6 @@ predicted = zeros(fSteps, N_test-wLen);
 nData = testData;
 for i=1:5
     predicted(i,:) = predict(theSVM, nData')';
-    disp(predicted(i,:))
     nData = [testData(2:wLen,:); predicted(i,:)];
 end
 
--- SVMpredictor.py
+++ SVMpredictor.py
@@ -20,7 +20,7 @@ N_train = dLen*N//2-dLen+1; #training data length - 149901
 wLen = 5*dLen; 
 N_test = dLen*N//2; #test data length - 150000
 N = N_train+N_test; #total data length - 299901
-
+sample_len = 5
 data = pd.read_csv('arp_dataset.txt',sep='\n')
 data = data[data.columns[0:1]].values
 datas = np.transpose(data)
@@ -55,11 +55,17 @@ label_reshape = trainLbl.reshape((N_train-1))
 train_reshape = trainData.transpose()
 testData_reshape = testData.transpose()
 clf = theSVR.fit(train_reshape,label_reshape)
-print(clf.predict(testData_reshape))
+
 fSteps = dLen; #tracks number of future steps to predict
+score = np.zeros((1,N_test))
 predicted = np.zeros((fSteps, N_test-wLen-1));
 
 nData = testData_reshape;
-for i in np.arange(0,5):
+for i in np.arange(0,sample_len):
     predicted[i] = clf.predict(nData);
-    #nData = [predicted[i]]
+    nData = np.concatenate((testData[0:wLen],predicted[i:sample_len])).reshape((wLen,-1))
+    #for r in np.arange(0,i):
+        #nData = [predicted[r]]
+
+for i in np.arange(sample_len):
+    score = predicted[i];
